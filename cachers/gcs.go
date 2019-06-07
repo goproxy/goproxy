09c1d800c4274b449d87860e1ea0f5c3205410ac
Aofei Sheng <aofei@aofeisheng.com>
2019-06-08 02:53:43 +0800

    refactor: use minio/minio-go to simplify `cachers.GCS`

--- cachers/gcs.go
+++ cachers/gcs.go
@@ -2,32 +2,25 @@ package cachers
 
 import (
 	"context"
-	"crypto/md5"
-	"errors"
 	"hash"
-	"io"
-	"os"
-	"path"
 	"sync"
-	"time"
 
-	"cloud.google.com/go/storage"
 	"github.com/goproxy/goproxy"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/option"
 )
 
 // GCS implements the `goproxy.Cacher` by using the Google Cloud Storage.
 type GCS struct {
-	// CredentialsJSON is the credentials JSON of the Google Cloud Platform.
+	// Endpoint is the endpoint of the Google Cloud Storage.
 	//
-	// If the `CredentialsJSON` is empty, then it will try to be read from
-	// the file targeted by the GOOGLE_APPLICATION_CREDENTIALS environment
-	// variable.
-	//
-	// Note that if you are running on the Google Cloud Platform, you
-	// usually do not need to provide the `CredentialsJSON`.
-	CredentialsJSON string `mapstructure:"credentials_json"`
+	// If the `Endpoint` is empty, the "https://storage.googleapis.com" is
+	// used.
+	Endpoint string `mapstructure:"endpoint"`
+
+	// AccessKey is the access key of the Google Cloud Platform.
+	AccessKey string `mapstructure:"access_key"`
+
+	// SecretKey is the secret key of the Google Cloud Platform.
+	SecretKey string `mapstructure:"secret_key"`
 
 	// BucketName is the name of the bucket.
 	BucketName string `mapstructure:"bucket_name"`
@@ -35,170 +28,40 @@ type GCS struct {
 	// Root is the root of the caches.
 	Root string `mapstructure:"root"`
 
-	loadOnce  sync.Once
-	loadError error
-	bucket    *storage.BucketHandle
+	loadOnce sync.Once
+	minio    *MinIO
 }
 
 // load loads the stuff of the m up.
 func (g *GCS) load() {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-
-	cos := []option.ClientOption{}
-	if g.CredentialsJSON != "" {
-		var creds *google.Credentials
-		if creds, g.loadError = google.CredentialsFromJSON(
-			ctx,
-			[]byte(g.CredentialsJSON),
-		); g.loadError != nil {
-			return
-		}
-
-		cos = append(cos, option.WithCredentials(creds))
+	endpoint := g.Endpoint
+	if endpoint == "" {
+		endpoint = "https://storage.googleapis.com"
 	}
 
-	var client *storage.Client
-	if client, g.loadError = storage.NewClient(
-		ctx,
-		cos...,
-	); g.loadError != nil {
-		return
+	g.minio = &MinIO{
+		Endpoint:        endpoint,
+		AccessKeyID:     g.AccessKey,
+		SecretAccessKey: g.SecretKey,
+		BucketName:      g.BucketName,
+		Root:            g.Root,
+		virtualHosted:   true,
 	}
-
-	g.bucket = client.Bucket(g.BucketName)
 }
 
 // NewHash implements the `goproxy.Cacher`.
 func (g *GCS) NewHash() hash.Hash {
-	return md5.New()
+	return g.minio.NewHash()
 }
 
 // Cache implements the `goproxy.Cacher`.
 func (g *GCS) Cache(ctx context.Context, name string) (goproxy.Cache, error) {
-	if g.loadOnce.Do(g.load); g.loadError != nil {
-		return nil, g.loadError
-	}
-
-	oh := g.bucket.Object(path.Join(g.Root, name))
-	attrs, err := oh.Attrs(ctx)
-	if err != nil {
-		if err == storage.ErrObjectNotExist {
-			return nil, goproxy.ErrCacheNotFound
-		}
-
-		return nil, err
-	}
-
-	return &gcsCache{
-		ctx:      ctx,
-		oh:       oh,
-		name:     name,
-		size:     attrs.Size,
-		modTime:  attrs.Updated,
-		checksum: attrs.MD5,
-	}, nil
+	g.loadOnce.Do(g.load)
+	return g.minio.Cache(ctx, name)
 }
 
 // SetCache implements the `goproxy.Cacher`.
 func (g *GCS) SetCache(ctx context.Context, c goproxy.Cache) error {
-	if g.loadOnce.Do(g.load); g.loadError != nil {
-		return g.loadError
-	}
-
-	w := g.bucket.Object(path.Join(g.Root, c.Name())).NewWriter(ctx)
-	w.ContentType = mimeTypeByExtension(path.Ext(c.Name()))
-	if _, err := io.Copy(w, c); err != nil {
-		return err
-	}
-
-	return w.Close()
-}
-
-// gcsCache implements the `goproxy.Cache`. It is the cache unit of the `GCS`.
-type gcsCache struct {
-	ctx      context.Context
-	oh       *storage.ObjectHandle
-	offset   int64
-	closed   bool
-	name     string
-	size     int64
-	modTime  time.Time
-	checksum []byte
-}
-
-// Read implements the `goproxy.Cache`.
-func (gc *gcsCache) Read(b []byte) (int, error) {
-	if gc.closed {
-		return 0, os.ErrClosed
-	} else if gc.offset >= gc.size {
-		return 0, io.EOF
-	}
-
-	r, err := gc.oh.NewRangeReader(gc.ctx, gc.offset, -1)
-	if err != nil {
-		return 0, err
-	}
-	defer r.Close()
-
-	n, err := r.Read(b)
-	gc.offset += int64(n)
-
-	return n, err
-}
-
-// Seek implements the `goproxy.Cache`.
-func (gc *gcsCache) Seek(offset int64, whence int) (int64, error) {
-	if gc.closed {
-		return 0, os.ErrClosed
-	}
-
-	switch whence {
-	case io.SeekStart:
-	case io.SeekCurrent:
-		offset += gc.offset
-	case io.SeekEnd:
-		offset += gc.size
-	default:
-		return 0, errors.New("invalid whence")
-	}
-
-	if offset < 0 {
-		return 0, errors.New("negative position")
-	}
-
-	gc.offset = offset
-
-	return gc.offset, nil
-}
-
-// Close implements the `goproxy.Cache`.
-func (gc *gcsCache) Close() error {
-	if gc.closed {
-		return os.ErrClosed
-	}
-
-	gc.closed = true
-
-	return nil
-}
-
-// Name implements the `goproxy.Cache`.
-func (gc *gcsCache) Name() string {
-	return gc.name
-}
-
-// Size implements the `goproxy.Cache`.
-func (gc *gcsCache) Size() int64 {
-	return gc.size
-}
-
-// ModTime implements the `goproxy.Cache`.
-func (gc *gcsCache) ModTime() time.Time {
-	return gc.modTime
-}
-
-// Checksum implements the `goproxy.Cache`.
-func (gc *gcsCache) Checksum() []byte {
-	return gc.checksum
+	g.loadOnce.Do(g.load)
+	return g.minio.SetCache(ctx, c)
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,6 @@
 module github.com/goproxy/goproxy
 
 require (
-	cloud.google.com/go v0.39.0
 	github.com/aliyun/aliyun-oss-go-sdk v1.9.8
 	github.com/baiyubin/aliyun-sts-go-sdk v0.0.0-20180326062324-cfa1a18b161f // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20190430165422-3e4dfb77656c // indirect
@@ -9,15 +8,11 @@ require (
 	github.com/minio/minio-go/v6 v6.0.28
 	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/smartystreets/assertions v1.0.0 // indirect
-	go.opencensus.io v0.22.0 // indirect
+	golang.org/x/crypto v0.0.0-20190605123033-f99c8df09eb5 // indirect
 	golang.org/x/mod v0.1.0
-	golang.org/x/net v0.0.0-20190606173856-1492cefac77f
-	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	golang.org/x/net v0.0.0-20190607181551-461777fb6f67
 	golang.org/x/sys v0.0.0-20190606203320-7fc4e5ec1444 // indirect
+	golang.org/x/text v0.3.2 // indirect
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
-	google.golang.org/api v0.6.0
-	google.golang.org/appengine v1.6.1 // indirect
-	google.golang.org/genproto v0.0.0-20190605220351-eb0b1bdb6ae6 // indirect
-	google.golang.org/grpc v1.21.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
