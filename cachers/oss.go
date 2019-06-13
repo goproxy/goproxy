a89862f32011e279495b925e6a756811c926c828
Aofei Sheng <aofei@aofeisheng.com>
2019-06-13 16:59:48 +0800

    refactor: use minio/minio-go to simplify `cachers.OSS`

--- cachers/oss.go
+++ cachers/oss.go
@@ -2,20 +2,9 @@ package cachers
 
 import (
 	"context"
-	"crypto/md5"
-	"encoding/hex"
-	"errors"
 	"hash"
-	"io"
-	"net/http"
-	"os"
-	"path"
-	"strconv"
-	"strings"
 	"sync"
-	"time"
 
-	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/goproxy/goproxy"
 )
 
@@ -40,9 +29,8 @@ type OSS struct {
 	// Root is the root of the caches.
 	Root string `mapstructure:"root"`
 
-	loadOnce  sync.Once
-	loadError error
-	bucket    *oss.Bucket
+	loadOnce sync.Once
+	minio    *MinIO
 }
 
 // load loads the stuff of the m up.
@@ -52,166 +40,29 @@ func (o *OSS) load() {
 		endpoint = "https://oss-cn-hangzhou.aliyuncs.com"
 	}
 
-	var client *oss.Client
-	if client, o.loadError = oss.New(
-		endpoint,
-		o.AccessKeyID,
-		o.AccessKeySecret,
-	); o.loadError != nil {
-		return
+	o.minio = &MinIO{
+		Endpoint:        endpoint,
+		AccessKeyID:     o.AccessKeyID,
+		SecretAccessKey: o.SecretKeySecret,
+		BucketName:      o.BucketName,
+		Root:            o.Root,
+		virtualHosted:   true,
 	}
-
-	o.bucket, o.loadError = client.Bucket(o.BucketName)
 }
 
 // NewHash implements the `goproxy.Cacher`.
 func (o *OSS) NewHash() hash.Hash {
-	return md5.New()
+	return o.minio.NewHash()
 }
 
 // Cache implements the `goproxy.Cacher`.
 func (o *OSS) Cache(ctx context.Context, name string) (goproxy.Cache, error) {
-	if o.loadOnce.Do(o.load); o.loadError != nil {
-		return nil, o.loadError
-	}
-
-	objectName := path.Join(o.Root, name)
-	if e, err := o.bucket.IsObjectExist(objectName); err != nil {
-		return nil, err
-	} else if !e {
-		return nil, goproxy.ErrCacheNotFound
-	}
-
-	h, err := o.bucket.GetObjectMeta(objectName)
-	if err != nil {
-		return nil, err
-	}
-
-	contentLength, err := strconv.ParseInt(h.Get("Content-Length"), 10, 64)
-	if err != nil {
-		return nil, err
-	}
-
-	lastModified, err := http.ParseTime(h.Get("Last-Modified"))
-	if err != nil {
-		return nil, err
-	}
-
-	checksum, err := hex.DecodeString(strings.Trim(h.Get("ETag"), `"`))
-	if err != nil {
-		return nil, err
-	}
-
-	return &ossCache{
-		bucket:     o.bucket,
-		objectName: objectName,
-		name:       name,
-		size:       contentLength,
-		modTime:    lastModified,
-		checksum:   checksum,
-	}, nil
+	o.loadOnce.Do(o.load)
+	return o.minio.Cache(ctx, name)
 }
 
 // SetCache implements the `goproxy.Cacher`.
 func (o *OSS) SetCache(ctx context.Context, c goproxy.Cache) error {
-	if o.loadOnce.Do(o.load); o.loadError != nil {
-		return o.loadError
-	}
-
-	return o.bucket.PutObject(
-		path.Join(o.Root, c.Name()),
-		c,
-		oss.ContentType(mimeTypeByExtension(path.Ext(c.Name()))),
-	)
-}
-
-// ossCache implements the `goproxy.Cache`. It is the cache unit of the `OSS`.
-type ossCache struct {
-	bucket     *oss.Bucket
-	objectName string
-	offset     int64
-	closed     bool
-	name       string
-	size       int64
-	modTime    time.Time
-	checksum   []byte
-}
-
-// Read implements the `goproxy.Cache`.
-func (oc *ossCache) Read(b []byte) (int, error) {
-	if oc.closed {
-		return 0, os.ErrClosed
-	} else if oc.offset >= oc.size {
-		return 0, io.EOF
-	}
-
-	rc, err := oc.bucket.GetObject(
-		oc.objectName,
-		oss.Range(oc.offset, oc.size),
-	)
-	if err != nil {
-		return 0, err
-	}
-	defer rc.Close()
-
-	n, err := rc.Read(b)
-	oc.offset += int64(n)
-
-	return n, err
-}
-
-// Seek implements the `goproxy.Cache`.
-func (oc *ossCache) Seek(offset int64, whence int) (int64, error) {
-	if oc.closed {
-		return 0, os.ErrClosed
-	}
-
-	switch whence {
-	case io.SeekStart:
-	case io.SeekCurrent:
-		offset += oc.offset
-	case io.SeekEnd:
-		offset += oc.size
-	default:
-		return 0, errors.New("invalid whence")
-	}
-
-	if offset < 0 {
-		return 0, errors.New("negative position")
-	}
-
-	oc.offset = offset
-
-	return oc.offset, nil
-}
-
-// Close implements the `goproxy.Cache`.
-func (oc *ossCache) Close() error {
-	if oc.closed {
-		return os.ErrClosed
-	}
-
-	oc.closed = true
-
-	return nil
-}
-
-// Name implements the `goproxy.Cache`.
-func (oc *ossCache) Name() string {
-	return oc.name
-}
-
-// Size implements the `goproxy.Cache`.
-func (oc *ossCache) Size() int64 {
-	return oc.size
-}
-
-// ModTime implements the `goproxy.Cache`.
-func (oc *ossCache) ModTime() time.Time {
-	return oc.modTime
-}
-
-// Checksum implements the `goproxy.Cache`.
-func (oc *ossCache) Checksum() []byte {
-	return oc.checksum
+	o.loadOnce.Do(o.load)
+	return o.minio.SetCache(ctx, c)
 }
--- go.mod
+++ go.mod
@@ -1,18 +1,12 @@
 module github.com/goproxy/goproxy
 
 require (
-	github.com/aliyun/aliyun-oss-go-sdk v1.9.8
-	github.com/baiyubin/aliyun-sts-go-sdk v0.0.0-20180326062324-cfa1a18b161f // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20190430165422-3e4dfb77656c // indirect
-	github.com/kr/pretty v0.1.0 // indirect
-	github.com/minio/minio-go/v6 v6.0.28
-	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/minio/minio-go/v6 v6.0.29
 	github.com/smartystreets/assertions v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20190605123033-f99c8df09eb5 // indirect
+	golang.org/x/crypto v0.0.0-20190611184440-5c40567a22f8 // indirect
 	golang.org/x/mod v0.1.0
-	golang.org/x/net v0.0.0-20190607181551-461777fb6f67
-	golang.org/x/sys v0.0.0-20190606203320-7fc4e5ec1444 // indirect
+	golang.org/x/net v0.0.0-20190611141213-3f473d35a33a
+	golang.org/x/sys v0.0.0-20190610200419-93c9922d18ae // indirect
 	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
