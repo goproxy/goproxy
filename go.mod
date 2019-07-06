c324b345122180dda5786dfb0439dd7771c03887
Aofei Sheng <aofei@aofeisheng.com>
2019-07-06 13:51:15 +0800

    test: add response_test.go

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ require (
 	github.com/gopherjs/gopherjs v0.0.0-20190430165422-3e4dfb77656c // indirect
 	github.com/minio/minio-go/v6 v6.0.31
 	github.com/smartystreets/assertions v1.0.0 // indirect
+	github.com/stretchr/testify v1.3.0
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
 	golang.org/x/mod v0.1.0
 	golang.org/x/net v0.0.0-20190628185345-da137c7871d7
---
+++ response_test.go
@@ -0,0 +1,107 @@
+package goproxy
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetResponseCacheControlHeader(t *testing.T) {
+	rec := httptest.NewRecorder()
+
+	assert.Empty(t, rec.Header().Get("Cache-Control"))
+
+	setResponseCacheControlHeader(rec, false)
+	assert.Equal(
+		t,
+		"must-revalidate, no-cache, no-store",
+		rec.Header().Get("Cache-Control"),
+	)
+
+	setResponseCacheControlHeader(rec, true)
+	assert.Equal(
+		t,
+		"max-age=31536000",
+		rec.Header().Get("Cache-Control"),
+	)
+}
+
+func TestResponseString(t *testing.T) {
+	rec := httptest.NewRecorder()
+
+	responseString(rec, "Foobar")
+	assert.Equal(t, http.StatusOK, rec.Code)
+	assert.Equal(
+		t,
+		"text/plain; charset=utf-8",
+		rec.HeaderMap.Get("Content-Type"),
+	)
+	assert.Equal(t, "Foobar", rec.Body.String())
+}
+
+func TestResponseStatusCode(t *testing.T) {
+	rec := httptest.NewRecorder()
+
+	responseStatusCode(rec, http.StatusOK)
+	assert.Equal(t, http.StatusOK, rec.Code)
+	assert.Equal(
+		t,
+		"text/plain; charset=utf-8",
+		rec.HeaderMap.Get("Content-Type"),
+	)
+	assert.Equal(t, "OK", rec.Body.String())
+}
+
+func TestResponseNotFound(t *testing.T) {
+	rec := httptest.NewRecorder()
+
+	responseStatusCode(rec, http.StatusNotFound)
+	assert.Equal(t, http.StatusNotFound, rec.Code)
+	assert.Equal(
+		t,
+		"text/plain; charset=utf-8",
+		rec.HeaderMap.Get("Content-Type"),
+	)
+	assert.Equal(t, "Not Found", rec.Body.String())
+}
+
+func TestResponseMethodNotAllowed(t *testing.T) {
+	rec := httptest.NewRecorder()
+
+	responseStatusCode(rec, http.StatusMethodNotAllowed)
+	assert.Equal(t, http.StatusMethodNotAllowed, rec.Code)
+	assert.Equal(
+		t,
+		"text/plain; charset=utf-8",
+		rec.HeaderMap.Get("Content-Type"),
+	)
+	assert.Equal(t, "Method Not Allowed", rec.Body.String())
+}
+
+func TestResponseInternalServerError(t *testing.T) {
+	rec := httptest.NewRecorder()
+
+	responseStatusCode(rec, http.StatusInternalServerError)
+	assert.Equal(t, http.StatusInternalServerError, rec.Code)
+	assert.Equal(
+		t,
+		"text/plain; charset=utf-8",
+		rec.HeaderMap.Get("Content-Type"),
+	)
+	assert.Equal(t, "Internal Server Error", rec.Body.String())
+}
+
+func TestResponseBadGateway(t *testing.T) {
+	rec := httptest.NewRecorder()
+
+	responseStatusCode(rec, http.StatusBadGateway)
+	assert.Equal(t, http.StatusBadGateway, rec.Code)
+	assert.Equal(
+		t,
+		"text/plain; charset=utf-8",
+		rec.HeaderMap.Get("Content-Type"),
+	)
+	assert.Equal(t, "Bad Gateway", rec.Body.String())
+}
