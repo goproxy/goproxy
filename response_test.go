0790b11fb75fc658dd0ce919e0d473db17dda764
Aofei Sheng <aofei@aofeisheng.com>
2022-12-18 00:55:27 +0800

    test: cover `responseSuccess`

--- response_test.go
+++ response_test.go
@@ -1,11 +1,15 @@
 package goproxy
 
 import (
+	"bytes"
 	"errors"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestSetResponseCacheControlHeader(t *testing.T) {
@@ -234,6 +238,147 @@ func TestResponseInternalServerError(t *testing.T) {
 	if b, err := ioutil.ReadAll(recr.Body); err != nil {
 		t.Fatalf("unexpected error %q", err)
 	} else if want := "internal server error"; string(b) != want {
+		t.Errorf("got %q, want %q", b, want)
+	}
+}
+
+type successResponseBody struct {
+	io.Reader
+
+	checksum []byte
+	modTime  time.Time
+}
+
+func (srb successResponseBody) Checksum() []byte {
+	return srb.checksum
+}
+
+func (srb successResponseBody) ModTime() time.Time {
+	return srb.modTime
+}
+
+func TestResponseSuccess(t *testing.T) {
+	req := httptest.NewRequest("", "/", nil)
+	rec := httptest.NewRecorder()
+	responseSuccess(
+		rec,
+		req,
+		strings.NewReader("foobar"),
+		"text/plain; charset=utf-8",
+		60,
+	)
+	recr := rec.Result()
+	if want := http.StatusOK; recr.StatusCode != want {
+		t.Errorf("got %d, want %d", recr.StatusCode, want)
+	}
+
+	recrCT := recr.Header.Get("Content-Type")
+	if want := "text/plain; charset=utf-8"; recrCT != want {
+		t.Errorf("got %q, want %q", recrCT, want)
+	}
+
+	recrCC := recr.Header.Get("Cache-Control")
+	if want := "public, max-age=60"; recrCC != want {
+		t.Errorf("got %q, want %q", recrCC, want)
+	}
+
+	recrLM := recr.Header.Get("Last-Modified")
+	if want := ""; recrLM != want {
+		t.Errorf("got %q, want %q", recrLM, want)
+	}
+
+	recrET := recr.Header.Get("ETag")
+	if want := ""; recrET != want {
+		t.Errorf("got %q, want %q", recrET, want)
+	}
+
+	if b, err := ioutil.ReadAll(recr.Body); err != nil {
+		t.Fatalf("unexpected error %q", err)
+	} else if want := "foobar"; string(b) != want {
+		t.Errorf("got %q, want %q", b, want)
+	}
+
+	req = httptest.NewRequest(http.MethodHead, "/", nil)
+	rec = httptest.NewRecorder()
+	responseSuccess(
+		rec,
+		req,
+		bytes.NewBuffer([]byte("foobar")),
+		"text/plain; charset=utf-8",
+		60,
+	)
+	recr = rec.Result()
+	if want := http.StatusOK; recr.StatusCode != want {
+		t.Errorf("got %d, want %d", recr.StatusCode, want)
+	}
+
+	recrCT = recr.Header.Get("Content-Type")
+	if want := "text/plain; charset=utf-8"; recrCT != want {
+		t.Errorf("got %q, want %q", recrCT, want)
+	}
+
+	recrCC = recr.Header.Get("Cache-Control")
+	if want := "public, max-age=60"; recrCC != want {
+		t.Errorf("got %q, want %q", recrCC, want)
+	}
+
+	recrLM = recr.Header.Get("Last-Modified")
+	if want := ""; recrLM != want {
+		t.Errorf("got %q, want %q", recrLM, want)
+	}
+
+	recrET = recr.Header.Get("ETag")
+	if want := ""; recrET != want {
+		t.Errorf("got %q, want %q", recrET, want)
+	}
+
+	if b, err := ioutil.ReadAll(recr.Body); err != nil {
+		t.Fatalf("unexpected error %q", err)
+	} else if want := ""; string(b) != want {
+		t.Errorf("got %q, want %q", b, want)
+	}
+
+	req = httptest.NewRequest("", "/", nil)
+	rec = httptest.NewRecorder()
+	responseSuccess(
+		rec,
+		req,
+		successResponseBody{
+			Reader:   strings.NewReader("foobar"),
+			checksum: []byte{0, 1, 2, 3},
+			modTime:  time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+		},
+		"text/plain; charset=utf-8",
+		60,
+	)
+	recr = rec.Result()
+	if want := http.StatusOK; recr.StatusCode != want {
+		t.Errorf("got %d, want %d", recr.StatusCode, want)
+	}
+
+	recrCT = recr.Header.Get("Content-Type")
+	if want := "text/plain; charset=utf-8"; recrCT != want {
+		t.Errorf("got %q, want %q", recrCT, want)
+	}
+
+	recrCC = recr.Header.Get("Cache-Control")
+	if want := "public, max-age=60"; recrCC != want {
+		t.Errorf("got %q, want %q", recrCC, want)
+	}
+
+	recrLM = recr.Header.Get("Last-Modified")
+	if want := "Sat, 01 Jan 2000 00:00:00 GMT"; recrLM != want {
+		t.Errorf("got %q, want %q", recrLM, want)
+	}
+
+	recrET = recr.Header.Get("ETag")
+	if want := `"AAECAw=="`; recrET != want {
+		t.Errorf("got %q, want %q", recrET, want)
+	}
+
+	if b, err := ioutil.ReadAll(recr.Body); err != nil {
+		t.Fatalf("unexpected error %q", err)
+	} else if want := "foobar"; string(b) != want {
 		t.Errorf("got %q, want %q", b, want)
 	}
 }
