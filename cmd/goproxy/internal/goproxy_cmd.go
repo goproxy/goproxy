e1d0b32a562b16ce3fcf84c4ec3fac067d725e74
Aofei Sheng <aofei@aofeisheng.com>
2023-12-25 13:28:13 +0800

    refactor(cmd/goproxy): move `internal.binaryVersion` to `internal.go` file

--- cmd/goproxy/internal/goproxy_cmd.go
+++ cmd/goproxy/internal/goproxy_cmd.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"runtime/debug"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -21,22 +20,4 @@ A minimalist Go module proxy handler.
 	cmd.SetHelpCommand(&cobra.Command{Hidden: true})
 	cmd.AddCommand(newServerCmd())
 	return cmd
-}
-
-// binaryVersion returns the version of the running binary.
-func binaryVersion() string {
-	info, ok := debug.ReadBuildInfo()
-	if !ok {
-		return ""
-	}
-	version := "Version: " + info.Main.Version + "\n"
-	for _, setting := range info.Settings {
-		switch setting.Key {
-		case "vcs.revision":
-			version += "Revision: " + setting.Value + "\n"
-		case "vcs.time":
-			version += "Build Time: " + setting.Value + "\n"
-		}
-	}
-	return version
 }
--- cmd/goproxy/internal/internal.go
+++ cmd/goproxy/internal/internal.go
@@ -1,9 +1,29 @@
 package internal
 
+import "runtime/debug"
+
 // Execute executes the goproxy command and returns exit code.
 func Execute() int {
 	if err := newGoproxyCmd().Execute(); err != nil {
 		return 1
 	}
 	return 0
+}
+
+// binaryVersion returns the version of the running binary.
+func binaryVersion() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return ""
+	}
+	version := "Version: " + info.Main.Version + "\n"
+	for _, setting := range info.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			version += "Revision: " + setting.Value + "\n"
+		case "vcs.time":
+			version += "Build Time: " + setting.Value + "\n"
+		}
+	}
+	return version
 }
